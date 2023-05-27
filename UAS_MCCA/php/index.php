e3310c7d0c4b35814c94ace080fd8bcf21819e77
azharsyahid18 <134616687+azharsyahid18@users.noreply.github.com>
2023-05-27 09:16:28 +0700

    Add files via upload

---
+++ UAS_MCCA/php/index.php
@@ -0,0 +1,21 @@
+<?php
+$hasil =" ";
+if(isset($_GET['bilanganA']) and isset($_GET['bilanganB']) ){
+	$bilanganA = $_GET['bilanganA'];
+	$bilanganB = $_GET['bilanganB'];
+
+	if(isset($_GET['tambah'])){
+	$hasil = $bilanganA + $bilanganB;
+	}
+	else if(isset($_GET['kurang'])){
+	$hasil = $bilanganA - $bilanganB;
+	}
+	else if(isset($_GET['kali'])){
+	$hasil = $bilanganA * $bilanganB;
+	}
+	else if(isset($_GET['bagi'])){
+	$hasil = $bilanganA / $bilanganB;
+	}
+}
+?>
+echo "Test UAS MCCA";
